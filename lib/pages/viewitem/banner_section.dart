66f9fc29386d611c35df27b026f176edcf5e5d06
Delarram <Delurram@gmail.com>
2022-08-18 13:18:13 +0630

    build custom text

--- lib/pages/viewitem/banner_section.dart
+++ lib/pages/viewitem/banner_section.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:moviedp/resource/dimens.dart';
+import 'package:moviedp/widgets/custom_text.dart';
 
 class BannerView extends StatelessWidget {
   const BannerView({Key? key}) : super(key: key);
@@ -6,7 +9,38 @@ class BannerView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://i.pinimg.com/originals/ee/50/de/ee50dec983d90280a90a40afbe5be29d.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+         Align(
+           alignment: Alignment.bottomLeft,
+           child: Column(
+             children: const [
+               CustomText(text: "The XMan 2013",
+                 fontSize:textHeadingOne,
+                 fontWeight: FontWeight.w500,
+                 color: Color(0xffFFFFFF),
+               ),
+               CustomText(text: "Offical Trailer",
+               fontSize: textHeadingOne,
+                 fontWeight: FontWeight.w500,
+               color: Color(0xffFFFFFF),
+               ),
+             ],
+           ),
+         ),
+          Align(
+            child: Icon(
+              Icons.play_circle
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/resource/dimens.dart
+++ lib/resource/dimens.dart
@@ -1,4 +1,6 @@
 ///generic dimens
 const mediumMargin = 16.0;
+const textHeadingOne = 24.0;
 
+///specific
 const bannerContainerHeight = 200.0;
