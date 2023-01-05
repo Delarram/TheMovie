24f458f4cda81c77533ea79df2321225d1b1ef52
Delarram <tunyanaing960@gmail.com>
2023-01-06 01:46:44 +0630

    refactoring

--- lib/pages/homepage/home_page.dart
+++ lib/pages/homepage/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:moviedp/resource/colors.dart';
 import 'package:moviedp/resource/dimens.dart';
-import 'package:provider/provider.dart';
 
 import '../../bloc/now_playing_bloc.dart';
 import 'homepage_section/banner_section_view.dart';
@@ -25,59 +24,60 @@ class HomePage extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<NowPlayingBloc>(
-      create: (BuildContext context) => NowPlayingBloc(),
-    child: Consumer<NowPlayingBloc>(builder: (context, bloc, child){
-      return Scaffold(
-        backgroundColor:const Color(0xff161A20),
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: primaryColor,
-          title: const Text(
-            "Discover",
-            style: TextStyle(fontWeight: FontWeight.w700),
-          ),
-          leading: const Icon(Icons.menu),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.only(right: mediumMargin),
-              child: Icon(Icons.search),
-            ),
-          ],
+    // return ChangeNotifierProvider<NowPlayingBloc>(
+    //   create: (BuildContext context) => NowPlayingBloc(),
+    // child: Consumer<NowPlayingBloc>(builder: (context, bloc, child){
+    //   return
+    // }),
+    // );
+    return Scaffold(
+      backgroundColor:const Color(0xff161A20),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: primaryColor,
+        title: const Text(
+          "Discover",
+          style: TextStyle(fontWeight: FontWeight.w700),
         ),
-        body: Container(
-            color: primaryColor,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 15),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const BannerSectionView(),
-                   const  SizedBox(height: marginLarge),
-                    const HorizontalMovieListView(),
-                    const SizedBox(height: marginLarge),
-                   const CheckMovieSection(),
-                    const SizedBox(height: marginLarge),
-                    MovieTabBarSection(movieListTabBar: movieListTabBar,
-                    ),
-                    const ShowCaseSection(),
-                    const SizedBox(
-                      height: mediumMargin2X,
-                    ),
-                    Container(
-                        height: bestActorContainerHeight,
-                        color: homeScreenBackground,
-                        child: const BestActorSection()),
-                    const SizedBox(
-                      height: mediumMargin2X,
-                    )
-                  ],
-                ),
+        leading: const Icon(Icons.menu),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: mediumMargin),
+            child: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Container(
+          color: primaryColor,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 15),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const BannerSectionView(),
+                  const  SizedBox(height: marginLarge),
+                  const HorizontalMovieListView(),
+                  const SizedBox(height: marginLarge),
+                  const CheckMovieSection(),
+                  const SizedBox(height: marginLarge),
+                  MovieTabBarSection(movieListTabBar: movieListTabBar,
+                  ),
+                  const ShowCaseSection(),
+                  const SizedBox(
+                    height: mediumMargin2X,
+                  ),
+                  Container(
+                      height: bestActorContainerHeight,
+                      color: homeScreenBackground,
+                      child: const BestActorSection()),
+                  const SizedBox(
+                    height: mediumMargin2X,
+                  )
+                ],
               ),
-            )),
-      );
-    }),
+            ),
+          )),
     );
   }
   }
